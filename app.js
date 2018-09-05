04317487a66b325e3fca8c25dbef6a6d5f3f606b
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2018-09-05 08:08:05 -0400

    Added loop on app.js, removed forEach on hbs in order to generate multiple files.

--- app.js
+++ app.js
@@ -4,42 +4,48 @@ const hbs = require('handlebars');
 const path = require('path');
 const data = require('./test-data.json');
 
-const compile = async function(templateName, data) {
+data.attendees.forEach((attendee) => {
 
-    const filePath = await path.join(process.cwd(), 'templates', `${templateName}.hbs`);
-    const html = await fs.readFile(filePath, 'utf-8');
-    return hbs.compile(html)(data);
+    const compile = async function(templateName, attendee) {
 
-};
+        const filePath = await path.join(process.cwd(), 'templates', `${templateName}.hbs`);
+        const html = await fs.readFile(filePath, 'utf-8');
+        return hbs.compile(html)(attendee);
+
+    };
 
 
-// data.attendees.forEach((dude) => {
+    (async function() {
+        try {
+    
+            i = 0
+            
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+    
+            const content = await compile('attendee-list', attendee);
+    
+            // console.log(data.attendees);
+    
+            await page.setContent(content);
+            await page.emulateMedia('screen');
+            await page.pdf({
+                path: `${attendee.last}.certificate.pdf`,
+                format: 'A4',
+                printBackground: true
+            });
 
-// })
+            i++;
+    
+            console.log('done!');
+            await browser.close();
+            process.exit();
+    
+    
+        } catch (e) {
+            console.log('error!', e);
+        }
 
-(async function() {
-    try {
+    })();
 
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-
-        const content = await compile('attendee-list', data);
-
-        console.log(data.attendees);
-
-        await page.setContent(content);
-        await page.emulateMedia('screen');
-        await page.pdf({
-            path: `${data.attendees[0].last}.certificate.pdf`,
-            format: 'A4',
-            printBackground: true
-        });
-
-        console.log('done!');
-        await browser.close();
-        process.exit();
-
-    } catch (e) {
-        console.log('error!', e);
-    }
-})();
+})
